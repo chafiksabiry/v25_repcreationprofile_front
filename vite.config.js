4407236ec5f4dceb8d5fccd15ff39ac9f8d2531b
BILON Ikram <bilonikram20@gmail.com>
2025-05-05 16:32:30 +0100

    change url of the MF in vite.config

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: 'https://repcreationwizard.harx.ai/',
+    base: 'https://rep-profile-creation.harx.ai/',
     plugins: [
       react({
         jsxRuntime: 'classic',
